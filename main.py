fe0f15f0e0bc841273d84c788d8684bffd7538cf
jenav <jenav@example.com>
2025-02-07 21:44:28 +0000

    finish bookbot lessons

---
+++ main.py
@@ -0,0 +1,34 @@
+def count_characters(text):
+    lower = text.lower()
+    abc = {}
+    for i in range(0, len(lower)):
+        if not lower[i].isalpha():
+            continue
+        if lower[i] in abc:
+            abc[lower[i]] += 1
+        else:
+            abc[lower[i]] = 1
+    return abc
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def main():
+    book = "books/frankenstein.txt"
+    print(f"--- Begin report of {book} ---")
+    with open(book) as f:
+        file_contents = f.read()
+        wc = count_words(file_contents)
+        print(f"{wc} words found in the document\n")
+        chars_dict = count_characters(file_contents)
+        sortd = sorted(chars_dict.items(), key=lambda x: x[1], reverse=True)
+        for tuple in sortd:
+            print(f"The '{tuple[0]}' character was found {tuple[1]} times")
+
+        print("--- End report ---")
+
+
+main()
